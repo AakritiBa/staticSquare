ba4b6ffdec477b75c651d33561bde1defb34b8e7
AakritiBa <71770614+AakritiBa@users.noreply.github.com>
2020-11-04 17:37:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,36 +1,39 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
+var car, wall;
+var speed, weight;
 
-var engine, world;
-var ground;
-var ball;
-
-function setup(){
-    var canvas = createCanvas(400,400);
-    engine = Engine.create();
-    world = engine.world;
-
-    var ground_options ={
-        isStatic: true
-    }
-
-    ground = Bodies.rectangle(200,390,200,20,ground_options);
-    World.add(world,ground);
-    var ball_options ={
-        restitution: 1.0
-    }
-    ball = Bodies.circle(200,100,20,ball_options) ;
-    World.add(world,ball);
-
-    console.log(ground);
+function setup() {
+  createCanvas(1600,400);
+  speed=random(55,90);
+  weight=random(400,1500);
+  car=createSprite(50,200,50,50);
+  car.velocityX=speed;
+  car.shapeColor=color(255);
+  wall=createSprite(600,200,60,height/2);
+  wall.shapeColor=color(80,80,80);
+  
 }
 
-function draw(){
-    background(0);
-    Engine.update(engine);
-    rectMode(CENTER);
-    rect(ground.position.x,ground.position.y,400,20);
-    ellipseMode(RADIUS);
-    ellipse(ball.position.x,ball.position.y,20,20);
+function draw() {
+  background(0); 
+  if(wall.x-car.x<(car.width+wall.width)/2) {
+    car.velociTYX=0;
+    var deformation=0.5*weight*speed
+  }
+  if(wall.x-car.x<(car.width+wall.width)/2){
+    car.veloctyX=0;
+    var deformation=0.5*weight*speed*speed/22509;
+    if(deformation>100){
+      car.shapeColor=color(255,0,0);
+    }
+    if(deformation<180 && deformation>100){
+      car.shapeColor=color(230,230,0);
+    }
+    if(deformation<100){
+      car.shapeColor=color(0,255,0)
+    }
+
+  }
+  car.collide(wall);
+  drawSprites();
+  
 }
